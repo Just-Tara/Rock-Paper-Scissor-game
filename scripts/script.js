1e270ff15a3cd1c5b9c35e4383da6acc10f2a44e
Just-Tara <olasojinifemi67@gmail.com>
2025-01-16 17:30:09 +0100

    script.js

--- scripts/script.js
+++ scripts/script.js
@@ -82,7 +82,6 @@ score = {
         `You
           <img src="images/${playerMove}-emoji.png"
              alt="rock-image" class="move-icon">`
-           
          `<img src="images/${computerMove}-emoji.png" 
             alt="rock-image" class="move-icon">
            Computer`;
